fe0f46af701aa28d7d0fde9ed8c62cd2dd7b5da5
Mohammad Ali <mohammadali40192@gmail.com>
2025-09-20 17:57:34 +0600

    first commit

---
+++ componants/countries/countries.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const countries = () => {
+  return <div>Hello world</div>;
+};
+
+export default countries;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "../componants/countries/countries";
+
+function App() {
+  return (
+    <>
+      <h1>React counties</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
